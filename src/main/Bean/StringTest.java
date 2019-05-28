53d53e185df1ea2cb2ec5e6b26aaae30adc1210d
fanrong <fan.rong@trs.com.cn>
2019-05-28 14:22:40 +0800

    提交总结

---
+++ src/main/Bean/StringTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @Author: fr
+ * @Date: 2019/5/24 15:35
+ */
+public class StringTest {
+
+    @Test
+    public void testString(){
+        String s = "JAVA编程思想";
+        System.out.println(s);
+    }
+}
